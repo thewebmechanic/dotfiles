1611bf3b6916f506bc1af6e701f9608141b7c189
thewebmechanic <80426417+thewebmechanic@users.noreply.github.com>
2023-05-02 20:05:20 +0300

    Add files via upload

---
+++ display.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --mode 1920x1080 --pos 640x1440 --rotate normal --output DP-1-0 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-1-1 --off --output HDMI-1-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-2 --off
